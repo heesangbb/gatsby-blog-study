c6184446ccb9d5fec0642a267ca900918f6c8a2b
heesangbb <heesangbb@gmail.com>
2021-11-26 17:30:17 +0900

    PostHead, PostHeadInfo, PostContent

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,12 @@
-import React, { FunctionComponent, useMemo } from "react"
-import styled from "@emotion/styled"
-import GlobalStyle from "components/Common/GlobalStyle"
-import Introduction from "components/Main/Introduction"
-import Footer from "components/Common/Footer"
 import CategoryList, { CategoryListProps } from "components/Main/CategoryList"
+import Introduction from "components/Main/Introduction"
 import PostList from "components/Main/PostList"
-import { PostListItemType } from "types/PostItem.types"
 import { graphql } from "gatsby"
 import { IGatsbyImageData } from "gatsby-plugin-image"
 import queryString, { ParsedQuery } from "query-string"
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-`
+import React, { FunctionComponent, useMemo } from "react"
+import { PostListItemType } from "types/PostItem.types"
+import Template from "../components/Common/Template"
 
 type IndexPageProps = {
   location: {
@@ -73,16 +65,14 @@ const IndexPage: FunctionComponent<IndexPageProps> = function ({
   )
 
   return (
-    <Container>
-      <GlobalStyle />
+    <Template>
       <Introduction profileImage={gatsbyImageData} />
       <CategoryList
         selectedCategory={selectedCategory}
         categoryList={categoryList}
       />
       <PostList selectedCategory={selectedCategory} posts={edges} />
-      <Footer />
-    </Container>
+    </Template>
   )
 }
 
